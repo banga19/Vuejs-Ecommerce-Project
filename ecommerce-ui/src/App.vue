592c316f148af5dde37ca43cb3dc1bdcb346694a
banga19 <bangalifof16@gmail.com>
2023-05-27 12:13:09 +0300

    add uncommited changes

--- ecommerce-ui/src/App.vue
+++ ecommerce-ui/src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app"></div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
