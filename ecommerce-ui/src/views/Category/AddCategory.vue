6aad4268bb88a45a24a4d0c9f7a68a79d6104824
banga19 <bangalifof16@gmail.com>
2023-05-30 17:03:56 +0300

    centered the category form

--- ecommerce-ui/src/views/Category/AddCategory.vue
+++ ecommerce-ui/src/views/Category/AddCategory.vue
@@ -1,35 +1,40 @@
 <template>
   <div class="container">
-    <!--Header for form starts below-->
-    <div class="row">
-      <div class="text-center">
-        <h3>Add Category</h3>
-      </div>
-    </div>
-
     <!--Start of form-->
     <div class="row">
-      <form>
-        <div class="form-group">
-          <label for="">Category Name</label>
-          <input type="text" class="form-control" />
-          <!--input form form for category name-->
-        </div>
+      <!--Center the form-->
+      <div class="col-3"></div>
+      <div class="col-6">
+        <form>
+          <!--Header for form starts below-->
+          <div class="row">
+            <div class="text-center">
+              <h3>Add Category</h3>
+            </div>
+          </div>
 
-        <div class="form-group">
-          <label for="">Category Description</label>
-          <input type="text" class="form-control" />
-          <!--input form form for category Description-->
-        </div>
+          <div class="form-group">
+            <label for="">Category Name</label>
+            <input type="text" class="form-control" />
+            <!--input form form for category name-->
+          </div>
 
-        <div class="form-group">
-          <label for="">Category Image</label>
-          <input type="text" class="form-control" />
-          <!--input form form for category image-->
-        </div>
+          <div class="form-group">
+            <label for="">Category Description</label>
+            <input type="text" class="form-control" />
+            <!--input form form for category Description-->
+          </div>
 
-        <button type="submit"></button>
-      </form>
+          <div class="form-group">
+            <label for="">Category Image</label>
+            <input type="text" class="form-control" />
+            <!--input form form for category image-->
+          </div>
+
+          <button type="submit"></button>
+        </form>
+      </div>
+      <div class="col-3"></div>
     </div>
   </div>
 </template>
