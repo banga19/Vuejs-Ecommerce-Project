23845244fd259e3fa1a819cfc3ca63d3ad5b74e2
banga19 <bangalifof16@gmail.com>
2023-05-30 16:49:13 +0300

    added form input tag in AddCategory view

--- ecommerce-ui/src/router/index.js
+++ ecommerce-ui/src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
   },
 
   {
-    path: "/admin/catyegory/add",
+    path: "/AddCategory",
     name: "AddCategory",
     component: AddCategory,
   },
--- ecommerce-ui/src/views/Category/AddCategory.vue
+++ ecommerce-ui/src/views/Category/AddCategory.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="AddCategory">
-    <p>Add Category</p>
+  <div class="container">
+    <!--Header for form starts below-->
+    <div class="row">
+      <div class="text-center">
+        <h3>Add Category</h3>
+      </div>
+    </div>
+
+    <!--Start of form-->
+    <div class="row">
+      <form>
+        <div class="form-group">
+          <label for="">Category Name</label>
+          <input type="text" class="form-control" />
+          <!--input form form for category name-->
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
